e5d5d11c567e7456c85772f6400c6c90e41586d8
Klebanowski <Konrad.Klebanowski@stargard-logistic.eu>
2026-03-12 12:00:35 +0100

    changes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='keto-v1';
+const CACHE='budzet-v1';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./index.html','./manifest.json'])));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).catch(()=>caches.match('./index.html'))))});
